79d15ea0372cedb9b7010b0e516c3e940ec0b323
Avinash Sarguru <avinashsarguru@gmail.com>
2019-09-04 10:30:03 -0500

    module 1 code challenge Avinash Sarguru

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,44 @@
 class Restaurant
   attr_reader :name
-
+  @@all = []
   def initialize(name)
     @name = name
+    @@all << self
   end
-
+  def self.all
+    #return all restaurant objects
+    @@all
+  end
+  def reviews
+    #go into the array of all the review objects
+    #create a new array of the resturant objects where the resturant variable of reviews refer to this object
+    #return array
+    Review.all.select{|obj| obj.restaurant == self}
+  end
+  def customers
+    #find an array of all the review objects that refer to this restaurant
+    #then create a new array of all customer variable of all those objects
+    #return all uniq values in the array
+    reviews.map{|obj| obj.customer}.uniq
+  end
+  def average_star_rating
+    #find all the reviews for restaurant
+    #add together all the ratings together
+    #divide the sum by the amount of reviews 
+    #return value
+    reviews.map{|obj| obj.rating}.sum / reviews.count  
+  end
+  def longest_review
+    #sort all of the reviews of this restaurant by how long there content
+    #get the obj with the longest content 
+    #extract the content
+    #return content
+    reviews.sort_by{|obj| obj.content}.last.content
+  end
+  def self.find_by_name(name)
+    #go into the array of all restaurants
+    #then find first object where the name varaible is equal to passed string
+    #return object
+    all.find{|obj| obj.name == name}
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -6,6 +6,16 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+c1 = Customer.new("Joe", "Johnson")
+c2 = Customer.new("Joe", "Ingles")
+c3 = Customer.new("Kyle", "Kuzma")
+c4 = Customer.new("Donovan", "Mitchell")
 
+r1 = Restaurant.new("Mcdonalds")
+r2 = Restaurant.new("Taco Bell")
+r3 = Restaurant.new("KFC")
+r4 = Restaurant.new("Crab Shack")
+
+review1 = Review.new(c1, r1, "It was good", 4)
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
