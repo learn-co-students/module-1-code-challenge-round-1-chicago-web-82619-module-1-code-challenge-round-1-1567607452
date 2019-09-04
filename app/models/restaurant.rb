7900493456b1070e7f6e03977e364357e8d58ffe
Andrew <andrewriznykua93@gmail.com>
2019-09-04 10:33:09 -0500

    Solutions

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,37 @@
 class Restaurant
-  attr_reader :name
+    attr_reader :name
 
-  def initialize(name)
-    @name = name
-  end
+    @@all = []
 
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+    
+    def all_reviews
+        #All reviews for current restaurant
+        all_reviews = Review.all.select {|review| review.restaurant == self}
+    end
+
+    def customers
+        #Collecting all names of customers who left the review for current restaurant
+        all_reviews.collect {|review| review.customer}
+    end
+
+    def average_star_rating
+        all_ratings = all_reviews.collect {|review| review.rating}
+        all_ratings.reduce(:+)/all_ratings.size
+    end
+
+    def longest_review
+        all_reviews.collect {|review| review.review}.max_by(&:length)
+    end
+
+    def self.find_by_name(name)
+        Restaurant.all.find {|restaurant| restaurant.name == name}
+    end
 end
